ca59be8dc0ec00c9ac6965d30d3ca40a83d849c2
aryamukherjee <arya99155@gmail.com>
2018-07-01 15:38:35 -0400

    more changes(login)

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 var passport = require('passport');
 
 router.get('/home', function(req, res) {
-    //console.log(req.isAuthenticated());
-    //if(req.isAuthenticated())
-    //{
+    console.log(req.isAuthenticated());
+    if(req.isAuthenticated())
+    {
         res.sendFile('index.html', {root: appRoot + '/public/views'});  
-    //}
-    //else
-    //{
-    //    res.redirect('/auth/login');
-    //}
+    }
+    else
+    {
+        res.redirect('/auth/login');
+    }
     
 });
 
